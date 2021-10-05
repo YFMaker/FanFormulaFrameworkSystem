c36dd7f7e2780437f31a433e102ba69ff1db802e
衣凡 <oncwnuJ12BlVjqtDHY92MYbFYnUw@git.weixin.qq.com>
2021-10-05 16:48:45 +0800

    基础信息维护

--- FanFormulaFramework/FanFormulaFramework.Public/BaseSystemInfo.Secret.cs
+++ FanFormulaFramework/FanFormulaFramework.Public/BaseSystemInfo.Secret.cs
@@ -9,5 +9,72 @@ namespace FanFormulaFramework.Public
     /// </summary>
     public partial class BaseSystemInfo
     {
+        /// <summary>
+        /// 密码错误锁定次数
+        /// </summary>
+        public static int PassWordErrorLockLimt = 10;
+
+        /// <summary>
+        /// 连续N次输错密码后 锁定登录周期（分钟）
+        /// 若为0 表示账户无效强制锁死
+        /// </summary>
+        public static int PassWordErrorLockCycle = 5;
+
+        /// <summary>
+        /// 是否判断大小写
+        /// </summary>
+        public static bool MatchCaps = true;
+
+        /// <summary>
+        /// 最多数据行数限制
+        /// </summary>
+        public static int TopDataBaseLimt = 200;
+
+        /// <summary>
+        /// 强类型密码管理
+        /// 密码包含 字母 数字 字符
+        /// </summary>
+        public static bool CheckPassWordStrength = false;
+
+        /// <summary>
+        /// 服务器端加密存储
+        /// </summary>
+        public static bool ServerEncryptPassWord = true;
+
+        /// <summary>
+        /// 密码长度限制
+        /// </summary>
+        public static int PassWordMiniLength = 8;
+
+        /// <summary>
+        /// 密码包含字母
+        /// </summary>
+        public static bool LetterInPassWord = true;
+
+        /// <summary>
+        /// 修改密码时间周期（月）
+        /// </summary>
+        public static int ChangePassWordTime = 3;
+
+        /// <summary>
+        /// 用户名长度限制
+        /// </summary>
+        public static int AccountNameMiniLength = 2;
+
+        /// <summary>
+        /// 密码加权
+        /// </summary>
+        public static string PassWordEncryptionKey = "this is password for password";
+
+        /// <summary>
+        /// 绝对管理员（大于全部等级设计）
+        /// 仅供服务端遠程連接使用
+        /// </summary>
+        public static readonly string ServerUserName = "management";
+        /// <summary>
+        /// 绝对管理员密码（大于全部等级设计）
+        /// 仅供服务端遠程連接使用
+        /// </summary>
+        public static readonly string ServerPassWord = "rootPassWord";
     }
 }
---
+++ FanFormulaFramework/FanFormulaFramework.Public/BaseSystemInfo.Server.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FanFormulaFramework.Public
+{
+    /// <summary>
+    /// 系统核心信息服务端
+    /// </summary>
+    public partial class BaseSystemInfo
+    {
+        /// <summary>
+        /// 主机地址
+        /// Host="192.168.0.1"
+        /// </summary>
+        public static string Host = string.Empty;
+
+        /// <summary>
+        /// 端口号
+        /// </summary>
+        public static int Port = 8988;
+
+        /// <summary>
+        /// 允许新用户注册
+        /// </summary>
+        public static bool NewUserRegister = true;
+
+        /// <summary>
+        /// 禁止用户重复登录
+        /// 只允许登录一次
+        /// </summary>
+        public static bool UserOnLineLock = true;
+
+        /// <summary>
+        /// 软件是否需要注册
+        /// </summary>
+        public static bool NeedRegister = true;
+
+        /// <summary>
+        /// 服务码
+        /// </summary>
+        public static string ServerRegisterKey = string.Empty;
+
+        /// <summary>
+        /// 服务端缓存机制
+        /// </summary>
+        public static bool ServerCache = false;
+
+        private static string systemcode = string.Empty;
+        /// <summary>
+        /// 系统设置
+        /// </summary>
+        public static string SystemCode
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(systemcode))
+                {
+                    ///TODO 配置处理
+                }
+                else
+                {
+                    systemcode = "Default";
+                }
+
+                return systemcode;
+            }
+
+            set
+            {
+                systemcode = value;
+            }
+        }
+
+        /// <summary>
+        /// 检测周期
+        /// 在线周期检测
+        /// 5分钟+40秒循环延误
+        /// </summary>
+        public static int OnLineTimeOut = 5 * 60 + 40;
+
+        /// <summary>
+        /// 每1分钟 检测在线情况
+        /// Message服务检测
+        /// </summary>
+        public static int OnLineCheck = 1 * 60;
+
+        /// <summary>
+        /// 锁不住记录循环次数（DB服务）
+        /// </summary>
+        public static int LockNoWaitCount = 5;
+
+        /// <summary>
+        /// 锁不住记录等待时长（DB服务）
+        /// </summary>
+        public static int LockNoWaitTickTime = 30;
+
+        /// <summary>
+        /// 上传文件目录
+        /// </summary>
+        public static string UploadDirectory = "UpLoadDocument/";
+
+        /// <summary>
+        /// 默认密码
+        /// </summary>
+        public static string DefaultPassWord = "123456";
+    }
+}

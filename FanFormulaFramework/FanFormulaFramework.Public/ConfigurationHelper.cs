2b499d64a657c18cd937e7c9ff9b305e5711226e
衣凡 <oncwnuJ12BlVjqtDHY92MYbFYnUw@git.weixin.qq.com>
2021-10-07 16:43:49 +0800

    开始编写配置录入

---
+++ FanFormulaFramework/FanFormulaFramework.Public/ConfigurationHelper.cs
@@ -0,0 +1,48 @@
+﻿using FanFormulaFramework.Util;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FanFormulaFramework.Public
+{
+    public  class ConfigurationHelper
+    {
+        public static void GetConfig()
+        {
+            BaseSystemInfo.SystemVerion = ConfigurationManagerd.Appsetting<string>("SystemVerion");
+            BaseSystemInfo.SystemCompany = ConfigurationManagerd.Appsetting<string>("SystemCompany");
+            BaseSystemInfo.SystemName = ConfigurationManagerd.Appsetting<string>("SystemName");
+            BaseSystemInfo.SystemInfoValue = ConfigurationManagerd.Appsetting<string>("SystemInfoValue");
+            ///注册码
+            BaseSystemInfo.SystemRegistrationCode = ConfigurationManagerd.Appsetting<string>("SystemRegistrationCode");
+            if (!string.IsNullOrEmpty(ConfigurationManagerd.Appsetting<string>("SystemDataBaseIsWeighted"))) 
+            {
+                BaseSystemInfo.SystemDataBaseIsWeighted = ConfigurationManagerd.Appsetting<bool>("SystemDataBaseIsWeighted");
+            }
+            if (!string.IsNullOrEmpty(ConfigurationManagerd.Appsetting<string>("SystemDataBaseIsWeightedType")))
+            {
+                BaseSystemInfo.SystemDataBaseIsWeightedType = ConfigurationManagerd.Appsetting<EncryptionType>("SystemDataBaseIsWeightedType");
+            }
+            if (!string.IsNullOrEmpty(ConfigurationManagerd.Appsetting<string>("HandleIsSave")))
+            {
+                BaseSystemInfo.HandleIsSave = ConfigurationManagerd.Appsetting<bool>("HandleIsSave");
+            }
+            else
+            {
+                BaseSystemInfo.HandleIsSave = true;
+            }
+            BaseSystemInfo.SystemName = ConfigurationManagerd.Appsetting<string>("SystemName");
+            BaseSystemInfo.SystemName = ConfigurationManagerd.Appsetting<string>("SystemName");
+            BaseSystemInfo.SystemName = ConfigurationManagerd.Appsetting<string>("SystemName");
+            BaseSystemInfo.SystemName = ConfigurationManagerd.Appsetting<string>("SystemName");
+            BaseSystemInfo.SystemName = ConfigurationManagerd.Appsetting<string>("SystemName");
+            BaseSystemInfo.SystemName = ConfigurationManagerd.Appsetting<string>("SystemName");
+            BaseSystemInfo.SystemName = ConfigurationManagerd.Appsetting<string>("SystemName");
+            BaseSystemInfo.SystemName = ConfigurationManagerd.Appsetting<string>("SystemName");
+            BaseSystemInfo.SystemName = ConfigurationManagerd.Appsetting<string>("SystemName");
+            BaseSystemInfo.SystemName = ConfigurationManagerd.Appsetting<string>("SystemName");
+            BaseSystemInfo.SystemName = ConfigurationManagerd.Appsetting<string>("SystemName");
+            BaseSystemInfo.SystemName = ConfigurationManagerd.Appsetting<string>("SystemName");
+        }
+    }
+}

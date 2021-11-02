9b584ac8cc6e96e6f923daba986c750d11c9afe6
衣凡 <oncwnuJ12BlVjqtDHY92MYbFYnUw@git.weixin.qq.com>
2021-11-02 16:38:35 +0800

    开始编写DB服务

---
+++ FanFormulaFramework/FanFormulaFramework.DBService/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FanFormulaFramework.DBService.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Error"] = "123";
+            return View();
+        }
+    }
+}

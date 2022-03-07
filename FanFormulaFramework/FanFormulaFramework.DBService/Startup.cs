4804b20a4295b992f33ae1642cef891b3259848f
衣凡 <oncwnuJ12BlVjqtDHY92MYbFYnUw@git.weixin.qq.com>
2022-03-07 17:00:46 +0800

    修改因微软更新的变化

--- FanFormulaFramework/FanFormulaFramework.DBService/Startup.cs
+++ FanFormulaFramework/FanFormulaFramework.DBService/Startup.cs
@@ -35,7 +35,7 @@ namespace FanFormulaFramework.DBService
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, Microsoft.AspNetCore.Hosting.IApplicationLifetime appLifetime)
+        public void Configure(IApplicationBuilder app, IHostApplicationLifetime appLifetime)
         {
 
             appLifetime.ApplicationStarted.Register(OnStarted);

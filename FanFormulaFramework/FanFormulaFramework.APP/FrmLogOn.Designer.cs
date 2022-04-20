76b12ba3fb586564e1d03ae7517c313fdf386b43
衣凡 <oncwnuJ12BlVjqtDHY92MYbFYnUw@git.weixin.qq.com>
2022-04-20 16:43:38 +0800

    制作登录界面

--- FanFormulaFramework/FanFormulaFramework.APP/FrmLogOn.Designer.cs
+++ FanFormulaFramework/FanFormulaFramework.APP/FrmLogOn.Designer.cs
@@ -29,10 +29,27 @@ namespace FanFormulaFramework.APP
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmLogOn));
+            this.SuspendLayout();
+            // 
+            // FrmLogOn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = global::FanFormulaFramework.APP.Properties.Resources.LOGON;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(948, 582);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmLogOn";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FrmLogOn";
+            this.TransparencyKey = System.Drawing.SystemColors.Control;
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- FanFormulaFramework/FanFormulaFramework.APP/Program.cs
+++ FanFormulaFramework/FanFormulaFramework.APP/Program.cs
@@ -21,7 +21,7 @@ namespace FanFormulaFramework.APP
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FrmLogOn());
         }
     }
 }
